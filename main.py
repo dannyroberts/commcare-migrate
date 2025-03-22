d9f0dcc0942bd9548804ece356455ac411de7270
Daniel Roberts <droberts@dimagi.com>
2025-03-21 19:10:47 -0500

    Lay groundwork for id mapping

--- main.py
+++ main.py
@@ -1,30 +1,29 @@
 import os
 import sys
-from itertools import islice
 
 import requests
 from commcare_export.checkpoint import CheckpointManager
 from commcare_export.cli import main as commcare_export
-from commcare_export.writers import SqlMixin
 from sqlalchemy import select
 
 DB = "postgresql://localhost:5432/det"
 
 
-def call_commcare_export(det_username, det_password, query):
-
+def call_commcare_export(username, apikey, query, commcare_hq, project):
+    print(username, apikey, query)
     commcare_export([
         "--auth-mode", "apikey",
-        "--username", det_username,
-        "--password", det_password,
+        "--username", username,
+        "--password", apikey,
         "--query", query,
         "--output-format", "sql",
         "--output", DB,
-        "--project", "droberts"
+        "--project", project,
+        "--commcare-hq", commcare_hq,
     ])
 
 
-def push_from_db_to_target(det_username, det_password, target_username, target_password, target_project_url):
+def push_from_db_to_target(det_username, det_password, target_username, target_password, target_url, target_project):
     with get_db_context(DB) as sql_db:
         table = sql_db.get_table('Forms')
         result = sql_db.Session().execute(
@@ -45,7 +44,7 @@ def push_from_db_to_target(det_username, det_password, target_username, target_p
                 continue
             xform = xform_response.text
 
-            response = post_form(xform, target_project_url, target_username, target_password,
+            response = post_form(xform, f'{target_url}/a/{target_project}', target_username, target_password,
                                  spoofed_submit_time=form_received_on)
             if response.status_code == 500:
                 print(f"There was an error on the target server processing form {form_id}")
@@ -62,8 +61,8 @@ def get_db_context(db_string):
     return CheckpointManager(DB, None, None, None, None)
 
 def post_form(xform, target_project_url, target_username, target_password, spoofed_submit_time=None):
-    url = f'{TARGET_PROJECT_URL}receiver/api/'
-    auth = (os.environ['TARGET_USERNAME'], os.environ['TARGET_PASSWORD'])
+    url = f'{target_project_url}/receiver/api/'
+    auth = (target_username, target_password)
     headers = {'Content-Type': 'text/html; charset=UTF-8'}
     if spoofed_submit_time:
         headers['X-SUBMIT-TIME'] = f'{spoofed_submit_time.isoformat()}Z'
@@ -73,13 +72,31 @@ def post_form(xform, target_project_url, target_username, target_password, spoof
 
 
 if __name__ == "__main__":
-    DET_USERNAME = os.getenv("DET_USERNAME")
-    DET_PASSWORD = os.getenv("DET_PASSWORD")
-    TARGET_PROJECT_URL = os.getenv("TARGET_PROJECT_URL")
+    SOURCE_URL = os.getenv("SOURCE_URL") or "https://www.commcarehq.org"
+    SOURCE_PROJECT = os.getenv("SOURCE_PROJECT")
+    SOURCE_USERNAME = os.getenv("SOURCE_USERNAME")
+    SOURCE_APIKEY = os.getenv("SOURCE_APIKEY")
+    TARGET_URL = os.getenv("TARGET_URL")
+    TARGET_PROJECT = os.getenv("TARGET_PROJECT")
     TARGET_USERNAME = os.getenv("TARGET_USERNAME")
     TARGET_PASSWORD = os.getenv("TARGET_PASSWORD")
+    TARGET_APIKEY = os.getenv("TARGET_APIKEY")
+
     if sys.argv[1] == 'fetch':
-        call_commcare_export(os.getenv("DET_USERNAME"), os.getenv("DET_PASSWORD"),
-                             './commcare-migrate-source.xlsx')
+        call_commcare_export(
+            SOURCE_USERNAME,
+            SOURCE_APIKEY,
+            query='./commcare-migrate-source.xlsx',
+            commcare_hq=SOURCE_URL,
+            project=SOURCE_PROJECT,
+        )
+    elif sys.argv[1] == 'fetch-target':
+        call_commcare_export(
+            TARGET_USERNAME,
+            TARGET_APIKEY,
+            query='./commcare-migrate-target.xlsx',
+            commcare_hq=TARGET_URL,
+            project=TARGET_PROJECT,
+        )
     elif sys.argv[1] == 'push':
-        push_from_db_to_target(DET_USERNAME, DET_PASSWORD, TARGET_USERNAME, TARGET_PASSWORD, TARGET_PROJECT_URL)
+        push_from_db_to_target(SOURCE_USERNAME, SOURCE_APIKEY, TARGET_USERNAME, TARGET_PASSWORD, TARGET_URL, TARGET_PROJECT)
